ea2a53025ed14cbf5fc47d5497c62559abec0404
Shamainb <shamainbhengesa60@gmail.com>
2023-11-21 11:25:20 -0800

    task0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+
+/**
+ * swap - function that swaps two numbers
+ * @x: number to be swapped
+ * @y: number to be swapped
+ * Return: Nothing
+ */
+void swap(int *x, int *y)
+{
+	int tmp = *x;
+
+	*x = *y;
+	*y = tmp;
+}
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: array of elements to be swapped
+ * @size: size of the array
+ * Return: Nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int swapped;
+
+		for (i = 0; i < size - 1; i++)
+		{
+			swapped  = 0;
+
+			for (j = 0; j < size - i - 1; j++)
+			{
+				if (array[j] > array[j + 1])
+				{
+					swap(&array[j], &array[j + 1]);
+					swapped = 1;
+				}
+			}
+			if (swapped == 0)
+			{
+				break;
+			}
+		}
+}
