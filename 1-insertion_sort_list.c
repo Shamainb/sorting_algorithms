f205c60e46ce52d2a6773c053daeb8993153514e
Shamainb <shamainbhengesa60@gmail.com>
2023-11-26 19:06:36 -0800

    task1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,59 @@
 #include "sort.h"
 
-listint_t *swap_list(listint_t *pre *cur)
+/**
+ * swap_list - function interchanges two successive nodes
+ * @pre: node before the key
+ * @key: node inserted at its right position
+ * Return: Nothing
+ */
+
+void swap_list(listint_t **pre, listint_t **key)
 {
-	
-	listint_t *temp1 = pre -> prev;
-	listint_t *temp2
+	listint_t *tmp;
+	listint_t *new;
+
+	tmp = (*key)->next;
+	new = (*pre)->prev;
+
+	if (tmp != NULL)
+		tmp->prev = (*pre);
+	if (new != NULL)
+		new->next = (*key);
+	(*key)->next = (*pre);
+	(*pre)->next = tmp;
+	(*key)->prev = new;
+	(*pre)->prev = (*key);
+}
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ *                       in ascending order using the Insertion sort algorithm
+ * @list: pointer to the head of the list
+ * Return: Nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *key, *pre;
+
+	if ((*list) == NULL || (*list)->next == NULL || list == NULL
+			|| (*list)->prev != NULL)
+		return;
+
+	current = (*list);
+	current = current->next;
+
+	while (current != NULL)
+	{
+		key = current;
+		pre = current->prev;
+		while (pre != NULL && ((pre->n) > (key->n)))
+		{
+			swap_list(&pre, &key);
+			pre = key->prev;
+			if (pre == NULL)
+				(*list) = key;
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
